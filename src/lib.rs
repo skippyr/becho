ce89240cf8d5139f55ad63fb35f0b65b03fa7a91
Sherman Rofeman <skippyr.developer@gmail.com>
2023-02-23 14:21:51 -0300

    chore: reorganize some texts.

--- src/lib.rs
+++ src/lib.rs
@@ -6,26 +6,60 @@ pub mod io;
 #[derive(Debug, Parser)]
 pub struct Arguments {
     /// Uses bold text.
-    #[arg(short = 'b', long = "bold", action)]
+    #[arg(
+        short = 'b',
+        long = "bold",
+        action,
+    )]
     pub is_bold: bool,
+
     /// Uses crossed out text.
-    #[arg(short = 'x', long = "crossed-out", action)]
+    #[arg(
+        short = 'x',
+        long = "crossed-out",
+        action,
+    )]
     pub is_crossed_out: bool,
+
     /// Uses italic text.
-    #[arg(short = 'i', long = "italic", action)]
+    #[arg(
+        short = 'i',
+        long = "italic",
+        action,
+    )]
     pub is_italic: bool,
+
     /// A color to be applied in the text.
-    #[arg(short = 'f', long = "foreground-color", default_value_t = String::from("normal"))]
+    #[arg(
+        short = 'f',
+        long = "foreground-color",
+        default_value_t = String::from("normal"),
+    )]
     pub foreground_color: String,
+
     /// A color to be applied in the background.
-    #[arg(short = 'g', long = "background-color", default_value_t = String::from("normal"))]
+    #[arg(
+        short = 'g',
+        long = "background-color",
+        default_value_t = String::from("normal"),
+    )]
     pub background_color: String,
+
     /// Uses dimmed color.
-    #[arg(short = 'd', long = "dim", action)]
+    #[arg(
+        short = 'd',
+        long = "dim",
+        action,
+    )]
     pub is_dimmed: bool,
+
     /// The separator used to concatenate text fragments.
-    #[arg(long = "separator", default_value_t = String::new())]
+    #[arg(
+        long = "separator",
+        default_value_t = String::new(),
+    )]
     pub separator: String,
+
     /// The text fragments to be handled.
     pub text_fragments: Vec<String>,
 }
