85c8d96cf6247190b5175aa31f4ca123e335056c
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-11 14:44:04 -0300

    chore: update colors script

--- scripts/examples/colors.sh
+++ scripts/examples/colors.sh
@@ -2,10 +2,20 @@
 
 main() {
   local -r text="Here are dragons!"
+  local -r colors=$(($(tput colors) - 1))
 
-  for color in $(seq 0 255); do
-    becho "(${color}) $(becho -f ${color} ${text})   $(becho -g ${color} ${text})"
+  becho "Use $(becho -bf cyan "Ctrl + c") to exit."
+  for color in $(seq 0 ${colors}); do
+    local counter="($(becho -bf red ${color})/$(becho -bf yellow ${colors})) "
+    becho "${counter}$(becho -f ${color} ${text})   $(becho -g ${color} ${text})"
+    becho -e "\033[2A"
+    sleep 0.2
   done
+
+  becho
+  becho
+  becho "You terminal could support colors from values 0 to ${colors}."
+  becho "That was $(becho -bf yellow cool), don't you think?"
 }
 
 main
